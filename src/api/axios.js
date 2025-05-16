a586a6c846e4c79cf159cc0407628642878c1721
Joren LeGoullon <lego5858joren@gmail.com>
2025-05-16 14:33:53 -0400

    fix: update API base URL for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: "https://localhost:7014",
-    headers: {
-        "Content-Type": "application/json",
+const baseURL = process.env.NODE_ENV === "production"
+  ? "https://people-api-fed4affrabfrawen.centralus-01.azurewebsites.net"
+  : "http://localhost:7014";
 
-    }
+export default axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
